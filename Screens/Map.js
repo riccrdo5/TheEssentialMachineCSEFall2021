9efc68c132d3241b069848ce267cb4fd4b1bb460
niharika211297 <77406463+niharika211297@users.noreply.github.com>
2021-10-21 03:46:38 -0400

    Google Maps Integration

--- Screens/Map.js
+++ Screens/Map.js
@@ -1,14 +1,39 @@
 import React from 'react';  
 import {Text, Image,ImageBackground} from 'react-native-elements';  
 import {View,Button,TouchableOpacity} from 'react-native';
+import { Linking, Platform } from 'react-native';
 import HomeScreen from './Homepage.js'; 
 import EditProfile from './EditProfile.js';
 import Receipts from './Receipts.js';
 import ScanQR from './ScanQR.js';
 
 import UserAccount from './UserAccount.js';
+import MapView, {PROVIDER_GOOGLE, Marker, Callout} from 'react-native-maps';
+
 
 export default class Map extends React.Component{
+    constructor(props) {
+        super(props);
+    }
+
+    markerClick =  (marker) => {
+        // alert(marker)
+        const mark = JSON.parse(marker);
+        const lat = mark.coordinate.latitude
+        const long = mark.coordinate.longitude
+        const scheme = 'https://www.google.com/maps/dir/?api=1&travelmode=walking&dir_action=navigate&destination='
+        const latLng = `${lat},${long}`;
+        const url =  `${scheme}${latLng}`
+        console.log(url)
+        Linking.canOpenURL(url).then(supported => {
+        if (!supported) {
+            console.log('Can\'t handle url: ' + url);
+        } else {
+            return Linking.openURL(url);
+        }
+        }).catch(err => console.error('An error occurred', err)); 
+    };              
+
     render(){
         const email = this.props.navigation.getParam('text');
         
@@ -23,7 +48,37 @@ export default class Map extends React.Component{
             </TouchableOpacity>
             </View>
 
-            <View style={{marginTop: 380, flexDirection:"row",justifyContent:'space-between', paddingLeft:25, paddingRight:25, paddingTop:200}}>
+            <MapView
+            style={{height:'50%', width:'90%', left:20, marginTop:70}}
+            provider ={PROVIDER_GOOGLE}
+            region={{
+                latitude: 42.880230,
+                longitude: 	-78.878738,
+                latitudeDelta: 0.01,
+                longitudeDelta:0.01
+            }}
+            showsUserLocation={true} 
+            showsMyLocationButton = {true}
+            >
+                <Marker 
+                    coordinate={{latitude: 43.044971, longitude: -78.761482}} 
+                    onPress= {e => this.markerClick(JSON.stringify(e.nativeEvent))}
+                    title={'Student Union'}>
+                </Marker>
+                <Marker 
+                    coordinate={{latitude: 43.027150, longitude: -78.806470}}
+                    title={'Capen Hall'}
+                    onPress={e => this.markerClick(JSON.stringify(e.nativeEvent))}>
+                </Marker>
+                 <Marker 
+                    coordinate={{latitude: 43.004560, longitude: -78.785910}}
+                    title={'Ellicot Complex'}
+                    onPress={e => this.markerClick(JSON.stringify(e.nativeEvent))}>
+                </Marker>
+
+            </MapView>
+
+            <View style={{flexDirection:"row",justifyContent:'space-between', paddingLeft:25, paddingRight:25, marginTop:80}}>
             <TouchableOpacity onPress={()=>{alert("Help!")}}>
             <Image source={require('./help-icon.png')} style={{height:80, width:90}} />
             </TouchableOpacity>
