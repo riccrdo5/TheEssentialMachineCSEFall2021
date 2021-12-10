8c4af6b5bf13d4bb2d1b5b2cb9000f361ce09b1d
shwetag9692 <91298730+shwetag9692@users.noreply.github.com>
2021-12-09 20:46:59 -0500

    Demo changes for bitcoin

--- Screens/PaymentOptions.js
+++ Screens/PaymentOptions.js
@@ -287,11 +287,20 @@ export default class PaymentOptions extends React.Component{
             </TouchableOpacity>
             
             <Text style={{paddingLeft:25, fontSize:25, fontWeight:'500', marginTop:20}}> Pay with: </Text>
-
+            
+                 { /*
             <View style={{flexDirection:"row",justifyContent:'space-between', paddingLeft:80, paddingRight:80, marginTop:10}}>
             <TouchableOpacity onPress={() => this.props.navigation.navigate('BitcoinPay', {text:this.state.amount, id:machineId , UserEmail:user})}>
                 <Image source={require('./bitcoin-icon.png')} style={{height:70, width:70}}/>
             </TouchableOpacity>
+            */ }
+
+            <View style={{flexDirection:"row",justifyContent:'space-between', paddingLeft:80, paddingRight:80, marginTop:10}}>
+            <TouchableOpacity onPress={() => this.props.navigation.navigate('BitcoinTestPay', {text:this.state.amount, id:machineId , UserEmail:user})}>
+                <Image source={require('./bitcoin-icon.png')} style={{height:70, width:70}}/>
+            </TouchableOpacity>
+
+
             <Modal
                     visible={this.state.showModal}
                     onRequestClose={() => this.setState({ showModal: false })}
